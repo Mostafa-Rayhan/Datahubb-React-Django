451aa59c5cae37d40c75d09c432ddb28c818e395
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-03-21 02:50:32 +0600

    add url

--- backend/authentication_system/authentication/urls.py
+++ backend/authentication_system/authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserRegistrationAPIView
+from .views import UserRegistrationAPIView, BlogListCreateAPIView, BlogDetailAPIView
 
 urlpatterns = [
     path('register/', UserRegistrationAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh_token/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('blogs/', BlogListCreateAPIView.as_view(), name='blog_list_create'),  # List and create blogs
+    path('blogs/<int:pk>/', BlogDetailAPIView.as_view(), name='blog_detail'),  # Retrieve, update, or delete a blog
 ]
